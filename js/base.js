6100922aa2b47ad9fd34a18c2a82be50255de40d
ljwrer <ljwrer@sina.com>
2015-06-26 17:31:38 +0800

    Regexp

--- js/base.js
+++ js/base.js
@@ -4,3 +4,41 @@ var print=function (value){
 var println=function (value){
 	print(value+"<br/>");
 }
+function typeOf(value) {
+	if (null === value) {
+		return 'null';
+	}
+
+	var type = typeof value;
+	if ('undefined' === type || 'string' === type) {
+		return type;
+	}
+
+	var typeString = toString.call(value);
+	switch (typeString) {
+		case '[object Array]':
+			return 'array';
+		case '[object Date]':
+			return 'date';
+		case '[object Boolean]':
+			return 'boolean';
+		case '[object Number]':
+			return 'number';
+		case '[object Function]':
+			return 'function';
+		case '[object RegExp]':
+			return 'regexp';
+		case '[object Object]':
+			if (undefined !== value.nodeType) {
+				if (3 == value.nodeType) {
+					return (/\S/).test(value.nodeValue) ? 'textnode' : 'whitespace';
+				} else {
+					return 'element';
+				}
+			} else {
+				return 'object';
+			}
+		default:
+			return 'unknow';
+	}
+}
